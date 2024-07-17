9fccec865b519d58fda884e1b2a7bedcdcfd2fae
Hamzaawan110376 <Muhammadhamzaawan110@gmail.com>
2024-07-18 02:38:13 +0500

    dtabases and tables created

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,11 +17,21 @@ export default authMiddleware({
       const customSubDomain= hostname.get('host')?.split(`${process.env.NEXT_PUBLIC_DOMAIN}`).filter(Boolean)[0]
 
       if (customSubDomain) {
-        return NextResponse.rewrite(new URL(`${customSubDomain} ${pathWithSearchParams}`))
+        return NextResponse.rewrite(new URL(`${customSubDomain} ${pathWithSearchParams}`, req.url))
         
       }
       if (url.pathname ==='/sign-in' || url.pathname ==='/sign-up') {
-        return NextResponse.redirect(new URL(``))
+        return NextResponse.redirect(new URL(`/agency/sign-in`, req.url))
+        
+      }
+
+            if (url.pathname ==='/' || url.pathname ==='/site' && url.host === process.env.NEXT_PUBLIC_DOMAIN) {
+        return NextResponse.rewrite(new URL(`/site`, req.url))
+        
+      }
+
+            if (url.pathname ==='/agency' || url.pathname ==='/subaccount' ) {
+        return NextResponse.rewrite(new URL(`${pathWithSearchParams}`, req.url))
         
       }
   },
