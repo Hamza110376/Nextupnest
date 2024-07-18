248897a5c922041a8d3b07bdfcbbdefd9793bae7
Hamzaawan110376 <Muhammadhamzaawan110@gmail.com>
2024-07-19 02:02:36 +0500

    quries addded

---
+++ src/lib/queries.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import { currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+import { redirect } from "next/navigation";
+import { SubAccount, User } from "@prisma/client";
+
+export const getAuthUserDetails = async () => {
+  const user = await currentUser();
+  if (!user) {
+    return;
+  }
+  const userData = await db.user.findUnique({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+    },
+    include: {
+      Agency: {
+        include: {
+          SidebarOption: true,
+          SubAccount: {
+            include: {
+              SidebarOption: true,
+            },
+          },
+        },
+      },
+      Permissions: true,
+    },
+  });
+  return userData;
+};
+
+export const activityLogNotification = async ({
+  agencyId,
+  description,
+  subaccountId,
+}: {
+  agencyId?: string;
+  description: string;
+  subaccountId?: string;
+}) => {
+  const authUser = await currentUser();
+  let userData;
+  if (authUser) {
+    const response = await db.user.findFirst({
+      where: {
+        Agency: {
+          SubAccount: {
+            some: { id: subaccountId },
+          },
+        },
+      },
+    });
+    if (response) userData = response;
+    {
+    }
+  }
+};
+
+export const createTeamUser = async (agencyId: string, user: User) => {
+  if (user.role === "AGENCY_OWNER") return null;
+  const response = await db.user.create({ data: { ...user } });
+  return response;
+};
+
+export const verifyAndAcceptInvitation = async () => {
+  const user = await currentUser();
+  if (!user) return redirect("/sign-in");
+  const invitationExist = await db.invitation.findUnique({
+    where: {
+      email: user.emailAddresses[0].emailAddress,
+      status: "PENDING",
+    },
+  });
+  if (invitationExist) {
+    const userDetail = await createTeamUser(invitationExist.agencyId, {
+      email: invitationExist.email,
+      agencyId: invitationExist.agencyId,
+      avatarUrl: user.imageUrl,
+      name: `${user.firstName} ${user.lastName}`,
+      role: invitationExist.role,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+};
